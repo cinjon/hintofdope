2589daa413ef5e3e2fdb2ce23744eb8fd1eaa7a4
cinjon <cinjon.resnick@gmail.com>
2015-07-31 14:00:40 -0700

    configvars.

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 CSRF_ENABLED = True
-SECRET_KEY = os.environ.get('DEATH_SECRET_KEY')
+SECRET_KEY = os.environ.get('DOPE_SECRET_KEY')
 basedir = os.path.abspath(os.path.dirname(__file__))
-baseurl = os.environ.get('DEATH_BASE_URL', None)
-SQLALCHEMY_DATABASE_URI = os.environ.get('DEATH_DATABASE_URL', os.environ.get('DATABASE_URL', None))
-TWILIO_ACCOUNT_SID = os.environ.get('LEPETITMORT_TWILIO_SID')
-TWILIO_AUTH_TOKEN = os.environ.get('LEPETITMORT_TWILIO_TOKEN')
+baseurl = os.environ.get('DOPE_BASE_URL', None)
+SQLALCHEMY_DATABASE_URI = os.environ.get('DOPE_DATABASE_URL', os.environ.get('DATABASE_URL', None))
+TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_SID')
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_TOKEN')
