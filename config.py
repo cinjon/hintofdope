f177a301cb7e66de8be7e55478daa68b78d9cceb
dinomiha <dino.mihalopoulos@gmail.com>
2015-11-28 15:39:15 -0500

    defaut postgres

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ SECRET_KEY = os.environ.get('DOPE_SECRET_KEY')
 basedir = os.path.abspath(os.path.dirname(__file__))
 baseurl = os.environ.get('DOPE_BASE_URL', None)
 SQLALCHEMY_DATABASE_URI = os.environ.get('DOPE_DATABASE_URL',
-                                         os.environ.get('DATABASE_URL', None))
+                                         os.environ.get('DATABASE_URL', 
+                                                        'postgresql+psycopg2://postgres@localhost:5432/hintofdope'))
 TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_SID')
 TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_ACC')
